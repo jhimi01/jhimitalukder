83898b265ad7fe77b9d17709ec12fe726661897e
jhimi01 <talukderjhimi@gmail.com>
2025-01-09 12:33:54 +0600

    added project and modify design

--- src/pages/Projects/ProjectCard.jsx
+++ src/pages/Projects/ProjectCard.jsx
@@ -1,6 +1,12 @@
 import LazyLoad from "react-lazy-load";
 
-const ProjectCard = ({ imageSrc, githubLink, liveLink, serverLink }) => (
+const ProjectCard = ({
+  projectname,
+  imageSrc,
+  githubLink,
+  liveLink,
+  serverLink,
+}) => (
   <div className="bg-white shadow-lg rounded-sm p-4">
     <LazyLoad effect="blur">
       <img
@@ -9,46 +15,51 @@ const ProjectCard = ({ imageSrc, githubLink, liveLink, serverLink }) => (
         alt="project screenshot"
       />
     </LazyLoad>
-    <div className="mt-4 w-full">
-      {githubLink && (
-        <h2 className="equal-word truncate text-gray-800">
-          github:
-          <a
-            target="_blank"
-            className="underline text-indigo-600"
-            href={githubLink}
-            rel="noopener noreferrer"
-          >
-            {githubLink}
-          </a>
-        </h2>
-      )}
-      {serverLink && (
-        <h2 className="equal-word truncate text-gray-800">
-          github-server:
-          <a
-            target="_blank"
-            className="underline text-indigo-600"
-            href={serverLink}
-            rel="noopener noreferrer"
-          >
-            {serverLink}
-          </a>
-        </h2>
-      )}
-      {liveLink && (
-        <h3 className="equal-word truncate text-gray-800">
-          live link:
-          <a
-            target="_blank"
-            className="underline text-indigo-600"
-            href={liveLink}
-            rel="noopener noreferrer"
-          >
-            {liveLink}
-          </a>
-        </h3>
-      )}
+    <div>
+      <h2 className="text-slate-700 my-2 font-semibold text-center">
+        {projectname}
+      </h2>
+      <div className="w-full flex justify-between flex-wrap">
+        {githubLink && (
+          <h2 className="equal-word bg-gray-300 p-1 text-sm hover:bg-gray-400 truncate text-gray-800">
+            <a
+              target="_blank"
+              className=""
+              href={githubLink}
+              title={githubLink}
+              rel="noopener noreferrer"
+            >
+              github-client
+            </a>
+          </h2>
+        )}
+        {liveLink && (
+          <h3 className="equal-word truncate bg-gray-300 p-1 text-sm hover:bg-gray-400 text-gray-800">
+            <a
+              target="_blank"
+              className=""
+              href={liveLink}
+              title={liveLink}
+              rel="noopener noreferrer"
+            >
+              live link
+            </a>
+          </h3>
+        )}
+        {serverLink && (
+          <h2 className="equal-word bg-gray-300 p-1 text-sm hover:bg-gray-400 truncate text-gray-800">
+            <a
+              target="_blank"
+              className=""
+              href={serverLink}
+              title={serverLink}
+              rel="noopener noreferrer"
+            >
+              github-server
+            </a>
+          </h2>
+        )}
+      </div>
     </div>
   </div>
 );
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -3,6 +3,8 @@ import "react-tabs/style/react-tabs.css";
 import "./Ptoject.css";
 import { Helmet } from "react-helmet";
 import ProjectCard from "./ProjectCard";
+import fb from "../../../public/fb.png";
+import yoga from "../../../public/doyoga.png";
 
 const Projects = () => {
   return (
@@ -21,29 +23,34 @@ const Projects = () => {
         </TabList>
         <hr />
 
+        {/* team project */}
         <TabPanel>
           <div className="flex items-center justify-center flex-wrap mt-5 gap-4">
             <ProjectCard
+              projectname={"Lang Master"}
               imageSrc="https://i.ibb.co/JrxvB4V/Screenshot-76566.png"
               githubLink="https://github.com/abcmehedi5/lang-master-client"
               liveLink="https://langmaster.netlify.app/"
             />
           </div>
         </TabPanel>
-
+        {/* simple js */}
         <TabPanel>
           <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-2 my-5">
             <ProjectCard
+              projectname={"Lienuxiy Cars"}
               imageSrc="https://i.ibb.co/1Ms1bqz/Screenshot-482.png"
               githubLink="https://github.com/jhimi01/collageAssignment.github.io"
               liveLink="https://jhimi01.github.io/collageAssignment.github.io/"
             />
             <ProjectCard
+              projectname={"Tour-travel"}
               imageSrc="/public/banner.png"
               githubLink="https://github.com/jhimi01/tour-travel"
               liveLink="https://jhimi01.github.io/tour-travel/"
             />
             <ProjectCard
+              projectname={"green."}
               imageSrc="https://i.ibb.co/YBT1VrP/Screenshot-483.png"
               githubLink="https://github.com/jhimi01/Blog.github.io"
               liveLink="https://jhimi01.github.io/Blog.github.io/"
@@ -51,29 +58,35 @@ const Projects = () => {
           </div>
         </TabPanel>
 
+        {/* react js */}
         <TabPanel>
           <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-2 my-5">
             <ProjectCard
+              projectname={"LogicLab"}
               imageSrc="https://i.ibb.co/7jLnJWz/Screenshot-484.png"
               githubLink="https://github.com/jhimi01/knowledge-cafe"
               liveLink="https://b7a8-logiclab-jhimi01.vercel.app/"
             />
             <ProjectCard
+              projectname={"DoctorCare"}
               imageSrc="https://i.ibb.co/7J5htB4/Screenshot-542.png"
               githubLink="https://github.com/jhimi01/doctors-care"
               liveLink="https://doctor-care-71f28.web.app/"
             />
             <ProjectCard
+              projectname={"Notes"}
               imageSrc="https://i.ibb.co/rvD89X1/Screenshot-485.png"
               githubLink="https://github.com/jhimi01/React-Notes"
               liveLink="https://react-notes-green.vercel.app/"
             />
             <ProjectCard
+              projectname={"Wings Bakers"}
               imageSrc="https://i.ibb.co/G907TMx/Screenshot-487.png"
               githubLink="https://github.com/jhimi01/wings-bakers-client"
               liveLink="https://wings-bakers-assignment10.web.app"
             />
             <ProjectCard
+              projectname={"Knowledge Store"}
               imageSrc="https://i.ibb.co/B2MZ4B5/Screenshot-486.png"
               githubLink="https://github.com/jhimi01/Knowledge-Store"
               liveLink="https://b7a9-career-hub-jhimi01.vercel.app/"
@@ -81,25 +94,36 @@ const Projects = () => {
           </div>
         </TabPanel>
 
+        {/* mernstack */}
         <TabPanel>
           <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-2 my-5">
             <ProjectCard
-              imageSrc="https://i.ibb.co/sP3Vbqb/Screenshot-854.png"
+              projectname={"DoYoga"}
+              imageSrc={yoga}
               githubLink="https://github.com/jhimi01/yoga-school"
               serverLink="https://github.com/jhimi01/yoga--school-server"
-              liveLink="https://yoga-school-7a5bc.web.app/"
+              liveLink="https://yoga-school.vercel.app/"
             />
             <ProjectCard
+              projectname={"Facebook"}
+              imageSrc={fb}
+              githubLink="https://github.com/jhimi01/facebook"
+              serverLink="https://github.com/jhimi01/facebook-server"
+              liveLink="https://facebook-green-nine.vercel.app/"
+            />
+            <ProjectCard
+              projectname={"College Service"}
               imageSrc="https://i.ibb.co/9cn1RkQ/Screenshot-541.png"
               githubLink="https://github.com/jhimi01/Colleges-service"
               serverLink="https://github.com/jhimi01/Colleges-service-server"
               liveLink="https://college-service-a34bb.web.app/"
             />
             <ProjectCard
+              projectname={"Doll Galaxy"}
               imageSrc="https://i.ibb.co/x5VWsNc/Screenshot-490.png"
               githubLink="https://github.com/jhimi01/doll-galaxy"
               serverLink="https://github.com/jhimi01/doll-galaxy-server"
-              liveLink="https://toys-galaxy.web.app"
+              liveLink="https://doll-galaxy.vercel.app/"
             />
           </div>
         </TabPanel>
@@ -108,11 +132,13 @@ const Projects = () => {
         <TabPanel>
           <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-2 my-5">
             <ProjectCard
+              projectname={"Maximo-shop"}
               imageSrc="https://i.ibb.co/ygfBrSj/Screenshot-200.png"
               githubLink="https://github.com/jhimi01/maximo-shop"
               liveLink="https://maximo-shop.vercel.app/"
             />
             <ProjectCard
+              projectname={"Venturer"}
               imageSrc="https://i.ibb.co/V307Z8N/Screenshot-201.png"
               githubLink="https://github.com/jhimi01/nextjs-blog"
               liveLink="https://nextjs-blog-omega-gilt.vercel.app/"
